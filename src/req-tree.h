a34a3ba7f15c54c3fb9ce40ee79eeeae4aee0618
Joshua Immanuel <josh@hipro.co.in>
2013-08-09 19:24:40 +0530

    requirement tree interface fixed to return id after adding child and sibling

--- src/req-tree.h
+++ src/req-tree.h
@@ -11,6 +11,8 @@
 #define HIPRO_THITTAM__9f2aa1b2_fe59_11e2_8d34_68a3c42125fd
 
 #include <memory>
+#include <string>
+#include <fstream>
 
 #include <boost/signals2.hpp>
 
@@ -25,15 +27,17 @@ public:
   typedef boost::signals2::signal <void (void)> signal_tree_dirty_t;
 
 public:
+  virtual signal_tree_dirty_t & signal_tree_dirty (void) = 0;
+
   /* Add the given child to the parent node. If parent node is nullptr
      then add it to the root node. If duplicate is true re-number the
-     reqid's in the given child node tree and add it. */
-  virtual void add_child (Requirement::ptr_t parent,
-                          Requirement::ptr_t new_req,
-                          bool duplicate) = 0;
-  virtual void add_sibling (Requirement::ptr_t sibling,
-                            Requirement::ptr_t new_req,
-                            bool duplicate) = 0;
+     reqid's in the given child node tree and return the new reqid. */
+  virtual std::string add_child (Requirement::ptr_t parent,
+                                 Requirement::ptr_t new_req,
+                                 bool duplicate) = 0;
+  virtual std::string add_sibling (Requirement::ptr_t sibling,
+                                   Requirement::ptr_t new_req,
+                                   bool duplicate) = 0;
 
   /* move operations. Return true if the operation is successful */
   virtual bool up_sibling (Requirement::ptr_t req) = 0;
@@ -48,12 +52,20 @@ public:
   virtual bool is_bottom_level (Requirement::ptr_t req) = 0;
 
   virtual int last_id (void) = 0;
+  virtual int height (void) = 0;
 
   virtual Requirement::ptr_t get (const std::string & reqid) = 0;
   virtual void detach (Requirement::ptr_t node) = 0;
 
   virtual bool is_dirty (void) = 0;
   virtual void clear_dirty (void) = 0;
+
+  /* populate the json string data */
+  virtual void load (const std::string & data) = 0;
+  /* populate the json data from the file handle */
+  virtual void load (const std::ifstream & file) = 0;
+  /* serialize the req-tree into json string */
+  virtual std::string serialize (void) = 0;
 
   /* virtual dtor */
   virtual ~ReqTree () {}
