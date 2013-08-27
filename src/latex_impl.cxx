7e91ee4e84279eaf4338e53d75ba73be6b19332e
Joshua Immanuel <josh@hipro.co.in>
2013-08-27 16:38:12 +0530

    latex flat generator added

---
+++ src/latex_impl.cxx
@@ -0,0 +1,172 @@
+/* Copyright (C) 2012,2013 HiPro IT Solutions Private Limited,
+ * Chennai. All rights reserved.
+ *
+ * This program and the accompanying materials are made available
+ * under the terms described in the LICENSE file which accompanies
+ * this distribution. If the LICENSE file was not attached to this
+ * distribution or for further clarifications, please contact
+ * legal@hipro.co.in. */
+
+#include "latex_impl.h"
+
+const char * LINK_ENTRY_TEMPLATE = "\\hyperref[req:%(reqid)]{%(reqid)}";
+const char * REQ_ENTRY_TEMPLATE = \
+  "\\%(top_level){%(reqid): %(title)}\n"
+  "\\label{req:%(reqid)}\n"
+  "\\begin{tabular}{ >{\\bfseries}r p{12cm} }\n"
+  "  Id: & %(reqid) \\\\\n"
+  "  Title: & %(title) \\\\\n"
+  "  Description: & %(description) \\\\\n"
+  "  Parent: & %(parent_link) \\\\\n"
+  "  Children: & %(children_links_list) \\\\\n"
+  "  Depends: & %(depends_links_list) \\\\\n"
+  "\\end{tabular}\n\n";
+
+LatexImpl::LatexImpl (HLogPtr logger) :
+  m_logger (logger)
+{
+
+}
+
+void
+LatexImpl::generate_flat (std::shared_ptr<Requirement> req,
+                          const std::string & top_level, std::ofstream * ofile)
+{
+  generator (req, top_level, ofile);
+  auto end = req->children_cend ();
+  for (auto it = req->children_cbegin (); it != end; ++it)
+    {
+      generate_flat (*it, top_level, ofile);
+    }
+}
+
+void
+LatexImpl::generate_topic_hierarchy (std::shared_ptr<Requirement> req,
+                                     std::ofstream * ofile)
+{
+
+}
+
+void
+LatexImpl::generator (std::shared_ptr<Requirement> req,
+                      const std::string & top_level, std::ofstream * ofile)
+{
+  if (req->id () == "root")
+    {
+      return;
+    }
+  std::map<std::string, std::string> params;
+  params["reqid"] = req->id ();
+  params["title"] = req->title ();
+  params["description"] = req->description ();
+
+  auto parent = req->parent ();
+  std::string str_plink ("None");
+  if (parent)
+    {
+      if (parent->id () != "root")
+        {
+
+          str_plink = expand_template (LINK_ENTRY_TEMPLATE, params);
+        }
+    }
+  params["parent_link"] = str_plink;
+  params["depends_links_list"] = generate_list (req->depends_cbegin (),
+                                                req->depends_cend (), params);
+  params["children_links_list"] = generate_list (req->children_cbegin (),
+                                                 req->children_cend (), params);
+  params["top_level"] = top_level;
+  auto data = expand_template (REQ_ENTRY_TEMPLATE, params);
+  ofile->write (data.c_str (), data.size ());
+}
+
+std::string
+LatexImpl::generate_list (Requirement::requirement_list_t::const_iterator begin,
+                          Requirement::requirement_list_t::const_iterator end,
+                          const std::map<std::string, std::string> & params)
+{
+  std::vector<std::string> slist;
+  for (auto it = begin; it != end; ++it)
+    {
+      slist.push_back (expand_template (LINK_ENTRY_TEMPLATE, params));
+    }
+  if (slist.size () == 0)
+    {
+      return "None";
+    }
+  return join (slist.begin (), slist.end (), ",");
+}
+
+std::string
+LatexImpl::expand_template (const std::string & tmpl_str,
+                            const std::map<std::string, std::string> & params)
+{
+  std::stringstream ss_data, ss_token;
+  bool token_identifier_found = false;
+  bool token_start_found = false;
+  const char token_identifier = '%';
+  const char token_start = '(';
+  const char token_end = ')';
+  auto it = tmpl_str.cbegin ();
+  auto end = tmpl_str.cend ();
+  while (it != end)
+    {
+      if (*it == token_identifier)
+        {
+          if (token_identifier_found == false)
+            {
+              token_identifier_found = true;
+              ss_token.clear ();
+              ss_token.str ("");
+              ++it;
+              continue;
+            }
+          else
+            {
+              ASSERT ((false), "Extra token identifier found inside a token");
+            }
+        }
+      if (token_identifier_found)
+        {
+          if (token_start_found == false)
+            {
+              if (*it == token_start)
+                {
+                  token_start_found = true;
+                }
+              else
+                {
+                  ASSERT ((false), "Token start missing after identifier");
+                }
+            }
+          else
+            {
+              if (*it == token_end)
+                {
+                  /* This will throw and die if the key is not in
+                     params */
+                  ss_data << params.at (ss_token.str ());
+                  token_identifier_found = false;
+                  token_start_found = false;
+                }
+              else
+                {
+                  ss_token << *it;
+                }
+            }
+        }
+      else
+        {
+          ss_data << *it;
+        }
+      ++it;
+    }
+  return ss_data.str ();
+}
+
+/*
+  Local Variables:
+  mode: c++
+  indent-tabs-mode: nil
+  End:
+*/
---
+++ src/latex_impl.h
@@ -0,0 +1,76 @@
+/* Copyright (C) 2012,2013 HiPro IT Solutions Private Limited,
+ * Chennai. All rights reserved.
+ *
+ * This program and the accompanying materials are made available
+ * under the terms described in the LICENSE file which accompanies
+ * this distribution. If the LICENSE file was not attached to this
+ * distribution or for further clarifications, please contact
+ * legal@hipro.co.in. */
+
+#ifndef HIPRO_THITTAM__9e1f695c_0ed9_11e3_a8a9_68a3c42125fd
+#define HIPRO_THITTAM__9e1f695c_0ed9_11e3_a8a9_68a3c42125fd
+
+#include <memory>
+#include <string>
+
+#include "common.h"
+#include "latex.h"
+#include "requirement.h"
+
+class LatexImpl : public Latex
+{
+public:
+  /*--- ctor/dtor ---*/
+  LatexImpl (HLogPtr logger);
+  ~LatexImpl () {}
+
+  /*--- methods required by the Latex interface ---*/
+  void generate_flat (
+    std::shared_ptr<Requirement> req, const std::string & top_level,
+    std::ofstream * ofile);
+
+  void generate_topic_hierarchy (
+    std::shared_ptr<Requirement> req, std::ofstream * ofile);
+
+private:
+  void generator (std::shared_ptr<Requirement> req,
+                  const std::string & top_level, std::ofstream * ofile);
+
+  std::string generate_list (
+    Requirement::requirement_list_t::const_iterator begin,
+    Requirement::requirement_list_t::const_iterator end,
+    const std::map<std::string, std::string> & params);
+
+  std::string expand_template (
+    const std::string & templ_str,
+    const std::map<std::string, std::string> & params);
+
+  /* Python equivalent join operation
+   * Courtesy: http://stackoverflow.com/a/6098417 */
+  template <typename Iter>
+  std::string join (Iter begin, Iter end, const std::string & separator)
+  {
+    std::stringstream result;
+    if (begin != end)
+      {
+        result << *begin++;
+      }
+    while (begin != end)
+      {
+        result << separator << *begin++;
+      }
+    return result.str ();
+  }
+
+private:
+  HLogPtr m_logger;
+};
+
+#endif // HIPRO_THITTAM__9e1f695c_0ed9_11e3_a8a9_68a3c42125fd
+
+/*
+  Local Variables:
+  mode: c++
+  indent-tabs-mode: nil
+  End:
+*/
