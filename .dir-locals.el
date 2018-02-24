86432571bdfccb9eb516f311d5172882b36557bd
Joe Steeve <js@hipro.co.in>
2018-02-24 13:28:23 +0530

    Add .dir-locals.el with proper cc-mode style

---
+++ .dir-locals.el
@@ -0,0 +1,13 @@
+((nil . ((indent-tabs-mode . nil)
+         (fill-column . 79))
+      )
+ (c++-mode . ((c-file-style . "gnu")
+              (c-basic-offset . 2)
+              (c-file-offsets . 
+                               ((substatement-open . 0)
+                                (arglist-intro . +)
+                                (arglist-close . +))
+                               ))
+           )
+
+ )
