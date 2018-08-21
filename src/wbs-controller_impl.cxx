99872962e08d7ac83fc79d415393c1097c2c93be
Joe Steeve <js@hipro.co.in>
2018-08-21 18:42:06 +0530

    untabify

--- src/wbs-controller_impl.cxx
+++ src/wbs-controller_impl.cxx
@@ -25,7 +25,7 @@ WBSControllerImpl::view_node_selected (
   {
     return;
   }
-  
+
   m_view->enable_cut (true);
   m_view->enable_copy (true);
   m_view->enable_delete (true);
@@ -65,9 +65,9 @@ WBSControllerImpl::view_add_child_clicked (void)
 void
 WBSControllerImpl::view_add_sibling_clicked (void)
 {
-	Log_I << "Adding Sibling";
-	m_view->add_sibling (m_selected_path);
-	m_view->renumber ();
+  Log_I << "Adding Sibling";
+  m_view->add_sibling (m_selected_path);
+  m_view->renumber ();
 }
 
 void
