ece492464fa712e70f75cd79fba7f2c4a952cdf1
Joe Steeve <js@hipro.co.in>
2018-08-21 18:55:49 +0530

    wbs: Update model on add-sibling

--- src/wbs-controller_impl.cxx
+++ src/wbs-controller_impl.cxx
@@ -68,6 +68,8 @@ WBSControllerImpl::view_add_sibling_clicked (void)
   Log_I << "Adding Sibling";
   m_view->add_sibling (m_selected_path);
   m_view->renumber ();
+
+  m_wbs->add_sibling (m_selected_path);
 }
 
 void
